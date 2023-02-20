6cb333ee20bf7452e99cf79b49ff07b297deff7c
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-02-20 12:08:18 -0500

    adding getSanityImageURL

--- astro/src/data/settings.js
+++ astro/src/data/settings.js
@@ -1,13 +1,13 @@
 export default {
-  title: `Astro Sanity Minimal Starter`,
+  title: `Mimotype`,
   description: `This is an example blog made with Astro.`,
-  url: `https://astro-sanity-minimal-starter.netlify.app`, // No trailing slash!
+  url: `https://mimotype-microsite.vercel.app`, // No trailing slash!
   // JSON LD
   name: `Example`,
   // Facebook URL
-  facebookURL: `https://facebook.com`,
+  // facebookURL: `https://facebook.com`,
   // Twitter URL
-  twitterURL: `https://twitter.com/jaydanurwin`,
+  // twitterURL: `https://twitter.com/jaydanurwin`,
   // Instagram URL
-  instagramURL: `https://instagram.com/jaydanurwin`,
+  // instagramURL: `https://instagram.com/jaydanurwin`,
 };
