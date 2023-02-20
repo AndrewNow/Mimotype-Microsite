8567f1b84cb4ac7d310ada9559ef218a2241bd47
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-02-20 13:47:55 -0500

    adding SSR

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import sanity from "astro-sanity";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   site: "https://mimotype-microsite.vercel.app/",
   // Your public domain, e.g.: https://my-site.dev/
   integrations: [
